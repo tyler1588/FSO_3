180678f20a65e29110461d72f3385de426fb8db1
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-07-08 13:29:19 -0400

    improve 3.9

--- part3/index.js
+++ part3/index.js
@@ -99,5 +99,8 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
   
-const PORT = 3001
-app.listen(PORT)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
