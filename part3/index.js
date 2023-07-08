c7e7fcf35293abc46794e0977a260655db75e04c
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-07-07 21:28:19 -0400

    complete 3.8

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
 
 app.use(express.json())
+app.use(morgan(':method :url :body'))
+
 
 let persons = [
     { 
